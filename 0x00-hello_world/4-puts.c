451054346b39c97e526220958b73a08143937125
ahmed1997623 <abazooka4@gmail.com>
2023-08-10 15:34:24 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - point "Programming is like building a multilingual puzzle,followed by a newline.
+   * return 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0); 
+}
